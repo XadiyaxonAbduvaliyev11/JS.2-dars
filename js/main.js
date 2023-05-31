cb18a57b07fe13e86765220e2673143e867c8486
aqdtjjksytjksfyksk <134036555+AnvarovFarxod@users.noreply.github.com>
2023-05-31 16:43:54 +0500

    Bu JS(Java Script) 2-dars kodlari
    Yuklandi

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/*
+let a= 44;
+let b= 43
+
+if (a == b){
+    console.log("a qiymati 44dan katta yoki teng!")
+}else {
+    console.log("a ning qiymati ko'rsatilgan songa katta yoki teng emas!")
+}*/
+
+
+let a=10;
+let b=46;
+let c=50;
+let d=55;
+/*
+
+if (a == 11 && b == 34 && c == 87 && d == 97 && e == 100){
+    console.log("Shartini hammasi bajarildi,bu ishlaydi")
+}
+else{
+    console.log("Shartlarning kamida 1tasi ishlamayapti")
+}*/
+
+/*
+if (a == 10 || b == 46 || c == 50 || d == 55){
+    console.log("Shartlarni kamida bittasi bajarildi")
+}
+else{
+    console.log("Shartlardan hammasi bajarilmadi")
+}*/
+
+
+/* JS data types: undefined,boolean,object */
+/*let soz = "text"; /!* >=string *!/
+let son = 45; /!* >=string *!/
+let matn;
+/!*let rost = 45==45?true:false;*!/
+
+if ( 45 == 44){
+   document.write("true")
+}else {
+   document.write("false")
+}*/
+
+/*let arrayObject = [45,78,"text","matn",true,false];
+let oddiyObject = {ismi:"Navruz",familiyasi:"Malikov",yoshi:17,talabami:"yoq"};
+let talabami = "Yo'q talaba emas"
+if (oddiyObject.yoshi > 10){
+    talabami = "Ha,talaba"
+}
+
+console.log("-Navruz talabami?" + talabami)*/
+
+
+let oddiyObject = {nomi:"Kompyuter",chiroylimi:"yoq"};
+let chiroylimi = "-Yo'q chiroyli emas"
+if (oddiyObject.chiroylimi > "ha"){
+    chiroylimi = "-Ha,juda ham"
+}
+
+console.log("-Kompyuter chiroylimi?" + chiroylimi)
+
+
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,20 @@
+let a=5+5-5*5;
+let b=6+6+6*6;
+let c=2+2-2/2;
+let d=9+9/9*9;
+
+
+if (a == 5+5-5*5  && b == 6+6+6*6 && c == 2+2-2/2 && d == 9+9/9*9){
+    console.log("Shartini hammasi bajarildi,bu ishlaydi")
+}
+else{
+    console.log("Shartlarning kamida 1tasi ishlamayapti")
+}
+
+
+if (a == 5+5-5 || b ==  6+6+6*6 || c == 2+2-2 || d == 9+9){
+    console.log("Shartlarni kamida bittasi bajarildi")
+}
+else {
+    console.log("Shartlardan hammasi bajarilmadi")
+}
